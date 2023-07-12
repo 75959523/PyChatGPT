dba3bc68682a30f3ab67efc3ee9a8271845da0e3
75959523 <54936383+75959523@users.noreply.github.com>
2023-07-13 00:30:56 +0800

    commit

--- util/logger_config.py
+++ util/logger_config.py
@@ -1,7 +1,8 @@
 import logging
 import pytz
-from concurrent_log_handler import ConcurrentRotatingFileHandler
+import os
 from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
 
 class TimeFormatter(logging.Formatter):
@@ -22,7 +23,33 @@ class TimeFormatter(logging.Formatter):
 formatter = TimeFormatter(fmt="%(asctime)s %(name)-15s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 
-def setup_logger(name, log_file, max_bytes=1024 * 1024 * 0.3, backup_count=100):
+class CustomRotatingFileHandler(RotatingFileHandler):
+    def doRollover(self):
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+        if self.backupCount > 0:
+            for i in range(self.backupCount - 1, 0, -1):
+                sfn = self._name_with_number(i)
+                dfn = self._name_with_number(i + 1)
+                if os.path.exists(sfn):
+                    if os.path.exists(dfn):
+                        os.remove(dfn)
+                    os.rename(sfn, dfn)
+            dfn = self._name_with_number(1)
+            if os.path.exists(dfn):
+                os.remove(dfn)
+            self.rotate(self.baseFilename, dfn)
+        if not self.delay:
+            self.stream = self._open()
+
+    def _name_with_number(self, number):
+        dir_name, base_name = os.path.split(self.baseFilename)
+        base_name, ext = os.path.splitext(base_name)
+        return os.path.join(dir_name, f"{number}{base_name}{ext}")
+
+
+def setup_logger(name, log_file, max_bytes=1024 * 1024 * 0.3, backup_count=10000):
     """Set up a logger that writes to a specified file.
 
     Args:
@@ -36,7 +63,7 @@ def setup_logger(name, log_file, max_bytes=1024 * 1024 * 0.3, backup_count=100):
     logger (logging.Logger): Configured logger.
     """
 
-    handler = ConcurrentRotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    handler = CustomRotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
     handler.setFormatter(formatter)
 
     console_handler = logging.StreamHandler()
